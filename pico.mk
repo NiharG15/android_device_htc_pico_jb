ca3f0b2eb9f875da26f30ba3c4bbe96f3c79f5ee
NiharG15 <nihargajre@gmail.com>
2012-12-16 18:51:34 +0530

    Update pico.mk
    For inline kernel building! 

--- pico.mk
+++ pico.mk
@@ -132,12 +132,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/pico/files/etc/vold.fstab:system/etc/vold.fstab 
 
-# Prebuilt Modules
-PRODUCT_COPY_FILES += \
-    device/htc/pico/prebuilt/bcmdhd.ko:system/lib/modules/bcmdhd.ko \
-    device/htc/pico/prebuilt/kineto_gan.ko:system/lib/modules/kineto_gan.ko \
-    device/htc/pico/prebuilt/cifs.ko:system/lib/modules/cifs.ko \
-    device/htc/pico/prebuilt/tun.ko:system/lib/modules/tun.ko \
+
     
 # Wifi
 PRODUCT_COPY_FILES += \
